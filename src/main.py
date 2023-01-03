1e0240f8851eba86d15e7a0470de420359b9f827
modernpacifist <modernpacifist@gmail.com>
2023-01-03 21:33:04 +0300

    03/01/2023-21:33:04 automated backup commit

--- src/main.py
+++ src/main.py
@@ -6,10 +6,8 @@ import logging
 # for local files/buffers parallel cleanup
 
 from dotenv import load_dotenv
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import Updater, MessageHandler, filters
 from telegram.constants import MAX_MESSAGE_LENGTH
-
-UPLOAD_LIMIT = 58
 
 load_dotenv()
 
@@ -19,86 +17,95 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
-def split_long_message(text: str) -> List[str]:
-    length = len(text)
-    if length < MAX_MESSAGE_LENGTH:
-        return [text]
+# def split_long_message(text: str) -> List[str]:
+    # length = len(text)
+    # if length < MAX_MESSAGE_LENGTH:
+        # return [text]
 
-    results = []
-    for i in range(0, length, MAX_MESSAGE_LENGTH):
-        results.append(text[i:MAX_MESSAGE_LENGTH])
+    # results = []
+    # for i in range(0, length, MAX_MESSAGE_LENGTH):
+        # results.append(text[i:MAX_MESSAGE_LENGTH])
 
-    return results
-
-
-def download_and_prep(file_name: str, message: Message) -> None:
-    message.voice.get_file().download(file_name)
-    message.reply_chat_action(action=ChatAction.TYPING)
+    # return results
 
 
-# TODO: this funciton must be rewritten to work with whisper <17-12-22, modernpacifist> #
-def transcribe(file_name: str, message: Message, lang_code: str = 'ru-RU', alternatives: List[str] = ['en-US', 'uk-UA']) -> List[str]:
-    media_info = MediaInfo.parse(file_name)
-    if len(media_info.audio_tracks) != 1 or not hasattr(media_info.audio_tracks[0], 'sampling_rate'):
-        os.remove(file_name)
-        raise ValueError('Failed to detect sample rate')
-    actual_duration = round(media_info.audio_tracks[0].duration / 1000)
-
-    sample_rate = media_info.audio_tracks[0].sampling_rate
-    encoding = RecognitionConfig.AudioEncoding.OGG_OPUS
-    if sample_rate not in SUPPORTED_SAMPLE_RATES:
-        message.reply_text('Your voice message has a sample rate of {} Hz which is not in the list '
-                           'of supported sample rates ({}).\n\nI will try to resample it, '
-                           'but this may reduce recognition accuracy'
-                           .format(sample_rate,
-                                   ', '.join(str(int(rate / 1000)) + ' kHz' for rate in SUPPORTED_SAMPLE_RATES)
-                                   ),
-                           quote=True)
-        message.reply_chat_action(action=ChatAction.TYPING)
-        encoding, file_name, sample_rate = resample(file_name)
-    config = RecognitionConfig(
-        encoding=encoding,
-        sample_rate_hertz=sample_rate,
-        enable_automatic_punctuation=True,
-        language_code=lang_code,
-        alternative_language_codes=alternatives,
-    )
-
-    try:
-        response = upload_to_gs(file_name, config) \
-            if actual_duration > UPLOAD_LIMIT \
-            else regular_upload(file_name, config)
-    except Exception as e:
-        print(e)
-        os.remove(file_name)
-        return ['Failed']
-
-    with conn.cursor() as cur:
-        cur.execute("insert into customer(user_id) values (%s) on conflict (user_id) do nothing;",
-                    (message.chat_id,))
-        cur.execute("update customer set balance = balance - (%s) where user_id = (%s);",
-                    (actual_duration, message.chat_id))
-        cur.execute("insert into stat(user_id, message_timestamp, duration) values (%s, current_timestamp, %s);",
-                    (message.chat_id, actual_duration))
-        conn.commit()
-
-    os.remove(file_name)
-
-    message_text = ''
-    for result in response.results:
-        message_text += result.alternatives[0].transcript + '\n'
-
-    return split_long_message(message_text)
+# def download_and_prep(file_name: str, message: Message) -> None:
+    # message.voice.get_file().download(file_name)
+    # message.reply_chat_action(action=ChatAction.TYPING)
 
 
-def voice_to_text(update: Update, context: CallbackContext) -> None:
+# # TODO: this funciton must be rewritten to work with whisper <17-12-22, modernpacifist> #
+# def transcribe(file_name: str, message: Message, lang_code: str = 'ru-RU', alternatives: List[str] = ['en-US', 'uk-UA']) -> List[str]:
+    # media_info = MediaInfo.parse(file_name)
+    # if len(media_info.audio_tracks) != 1 or not hasattr(media_info.audio_tracks[0], 'sampling_rate'):
+        # os.remove(file_name)
+        # raise ValueError('Failed to detect sample rate')
+    # actual_duration = round(media_info.audio_tracks[0].duration / 1000)
+
+    # sample_rate = media_info.audio_tracks[0].sampling_rate
+    # encoding = RecognitionConfig.AudioEncoding.OGG_OPUS
+    # if sample_rate not in SUPPORTED_SAMPLE_RATES:
+        # message.reply_text('Your voice message has a sample rate of {} Hz which is not in the list '
+                           # 'of supported sample rates ({}).\n\nI will try to resample it, '
+                           # 'but this may reduce recognition accuracy'
+                           # .format(sample_rate,
+                                   # ', '.join(str(int(rate / 1000)) + ' kHz' for rate in SUPPORTED_SAMPLE_RATES)
+                                   # ),
+                           # quote=True)
+        # message.reply_chat_action(action=ChatAction.TYPING)
+        # encoding, file_name, sample_rate = resample(file_name)
+    # config = RecognitionConfig(
+        # encoding=encoding,
+        # sample_rate_hertz=sample_rate,
+        # enable_automatic_punctuation=True,
+        # language_code=lang_code,
+        # alternative_language_codes=alternatives,
+    # )
+
+    # try:
+        # response = upload_to_gs(file_name, config) \
+            # if actual_duration > UPLOAD_LIMIT \
+            # else regular_upload(file_name, config)
+    # except Exception as e:
+        # print(e)
+        # os.remove(file_name)
+        # return ['Failed']
+
+    # with conn.cursor() as cur:
+        # cur.execute("insert into customer(user_id) values (%s) on conflict (user_id) do nothing;",
+                    # (message.chat_id,))
+        # cur.execute("update customer set balance = balance - (%s) where user_id = (%s);",
+                    # (actual_duration, message.chat_id))
+        # cur.execute("insert into stat(user_id, message_timestamp, duration) values (%s, current_timestamp, %s);",
+                    # (message.chat_id, actual_duration))
+        # conn.commit()
+
+    # os.remove(file_name)
+
+    # message_text = ''
+    # for result in response.results:
+        # message_text += result.alternatives[0].transcript + '\n'
+
+    # return split_long_message(message_text)
+
+
+def download_voice(update, context) -> None:
+    new_file = context.bot.get_file(update.message.voice.file_id)
+
+    new_file.download(f"{update.effective_message.chat.id}_{update.message.from_user.id}{update.message.message_id}.ogg")
+
+    update.message.reply_text("Voice note saved")
+
+
+def voice_to_text(update, context) -> None:
     message = update.effective_message
     # if message.voice.duration > MY_NERVES_LIMIT:
         # message.reply_text(POLITE_RESPONSE, quote=True)
         # return
 
     chat_id = update.effective_message.chat.id
-    file_name = '%s_%s%s.ogg' % (chat_id, update.message.from_user.id, update.message.message_id)
+    # file_name = '%s_%s%s.ogg' % (chat_id, update.message.from_user.id, update.message.message_id)
+    file_name = f"{chat_id}_{update.message.from_user.id}{update.message.message_id}.ogg"
     download_and_prep(file_name, message)
 
     transcriptions = transcribe(file_name, update.message)
@@ -111,6 +118,7 @@ def voice_to_text(update: Update, context: CallbackContext) -> None:
 
     for transcription in transcriptions:
         message.reply_text(transcription, quote=True)
+    message.reply_text("hehe")
 
 
 def unknown_text(update, context) -> None:
@@ -122,8 +130,8 @@ def error(update, context) -> None:
 
 
 def _add_handlers(dispatcher) -> None:
-    dispatcher.add_handler(MessageHandler(Filters.voice, voice_to_text))
-    dispatcher.add_handler(MessageHandler(Filters.text, unknown_text))
+    dispatcher.add_handler(MessageHandler(filters.Filters.voice, download_voice))
+    dispatcher.add_handler(MessageHandler(filters.Filters.text, unknown_text))
     dispatcher.add_error_handler(error)
 
 
